06627d75caeaf85a6b56bf896250226da9a179dd
Moser Charles-Henri <charles-henri.moser@eduvaud.ch>
2024-10-01 11:32:11 +0200

    Ajout dossier client

---
+++ php/client/api/readMessages.js
@@ -0,0 +1,58 @@
+
+async function readMessages() {
+    try {
+        const url = ""; // TODO : A compléter l'URL backend de la fonction pour lire les messages dans la base de données.
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Erreur lors de la récupération des messages.');
+        }
+        const messages = await response.json();
+        console.log('Messages reçus :');
+        console.log(messages);
+        
+        const messagesDiv = document.getElementById('idMessages');
+        messagesDiv.innerHTML = "";
+        
+        const table = document.createElement("table");
+        messagesDiv.appendChild(table);
+        const headerRow = document.createElement("tr");
+        
+        const thIp = document.createElement("th");
+
+        thIp.textContent = "IP Source";
+        headerRow.appendChild(thIp);
+        
+        const thMessage = document.createElement("th");
+        thMessage.textContent = "Messages";
+        headerRow.appendChild(thMessage);
+        
+        table.appendChild(headerRow);
+        
+        messages.forEach(m => {
+            
+            // TODO : à compléter on souhaite afficher chacun des messages dans la table (une row par message, 
+            // une colonne pour l'ip et une colonne pour le message construit à partir des ses caractères reconcaténés.
+            const row = document.createElement("tr");
+
+            const ipCell = document.createElement("td");
+            ipCell.textContent = m.ip;  // Assuming 'm.ip' is the IP address
+            row.appendChild(ipCell);
+
+            const messageCell = document.createElement("td");
+            messageCell.textContent = m.characters.join('');  // Assuming 'm.characters' is an array of characters
+            row.appendChild(messageCell);
+
+            table.appendChild(row);
+
+        });
+        
+    } catch (error) {
+        console.error('Erreur :', error.message);
+    }
+}
+
+// Toutes les 10 secondes, on collecte les message de la base de données.
+setInterval(readMessages, 10000);
+
+// Avec une lecture initiale...
+document.addEventListener('DOMContentLoaded', readMessages);
---
+++ php/client/api/telegraph_api.js
@@ -0,0 +1,102 @@
+let pressStartTime = 0;
+let currentBitPos = 0;
+let bitsSaved = [0, 0, 0, 0, 0, 0, 0, 0];
+let tabAscii = [];
+const bitShow = document.getElementById("bitsSaved");
+const messageASCII = document.getElementById("messageASCII");
+
+async function playSound(telegraphAudio) {
+  telegraphAudio.currentTime = 0;
+  try {
+    await telegraphAudio.play();
+  } catch (error) {
+    console.error("Erreur lors de la lecture audio :", error);
+  }
+}
+
+function stopSound(telegraphAudio) {
+  telegraphAudio.pause();
+}
+
+function stopTimer() {
+  const pressDuration = Date.now() - pressStartTime; // Calcule la durée
+  return pressDuration;
+}
+
+function startTimer() {
+  pressStartTime = Date.now();
+}
+
+async function changeToDown(telegraphButton, telegraphAudio) {
+  telegraphButton.style.backgroundImage = "url('media/telegraph_down.png')";
+
+  await playSound(telegraphAudio);
+}
+
+function sampleBit(timePressed) {
+  /* TODO : A compléter */
+  if (timePressed < 200) {
+    return 0;
+  } else {
+    return 1;
+  }
+
+  //return currentBit;
+}
+
+function changeToUp(telegraphButton, telegraphAudio) {
+  telegraphButton.style.backgroundImage = "url('media/telegraph_up.png')";
+
+  stopSound(telegraphAudio);
+}
+
+function accumulateBits(newBit) {
+  /* TODO : A compléter */
+  if (currentBitPos < 8) {
+    bitsSaved[currentBitPos] = newBit;
+    currentBitPos++;
+
+    bitShow.innerHTML += newBit += " "; // Montre les bits
+  }
+
+  if (currentBitPos === 8) {
+    const asciiChar = calculateAscii(bitsSaved);
+    console.log("ASCII Character:", asciiChar);
+    messageASCII.innerHTML += asciiChar;
+
+    tabAscii.push(asciiChar); // Add the character to tabAscii array
+    console.log("Complete ASCII Message:", tabAscii.join(""));
+
+    clearBitsSaved(); // Clear pour prochain bit
+    currentBitPos = 0; // Remet à 0 le nombre de bits
+
+    bitShow.innerHTML = ""; //Remet à rien les bits
+  }
+}
+
+function calculateAscii(bitsArray) {
+  /* TODO : A compléter */
+  let asciiValue = 0;
+  for (let i = 0; i < bitsArray.length; i++) {
+    asciiValue += bitsArray[i] * Math.pow(2, 7 - i); // Converti la ligne de binaire en décimal
+  }
+  return String.fromCharCode(asciiValue); // Converti les décimal en Symbole ASCII
+}
+
+if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+  module.exports = {
+    playSound,
+    stopSound,
+    changeToDown,
+    changeToUp,
+  };
+}
+
+// Ne pas modifier ces deux fonctions
+function getBitsSaved() {
+  return bitsSaved;
+}
+
+function clearBitsSaved() {
+  bitsSaved = [0, 0, 0, 0, 0, 0, 0, 0];
+}
